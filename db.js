23a363420e04c59a6102b69e8bff8aa64068ac7c
Anirudh Sharma <sharmaanirudh230@gmail.com>
2026-06-12 05:55:41 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -1,3 +1,23 @@
+<<<<<<< HEAD
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Anirudh1@",
+    database: "supermart_db"
+});
+
+db.connect(err => {
+    if (err) {
+        console.log("MySQL connection error:", err);
+    } else {
+        console.log("MySQL Connected");
+    }
+});
+
+module.exports = db;
+=======
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
@@ -16,3 +36,4 @@ db.connect(err => {
 });
 
 module.exports = db;
+>>>>>>> 3618986810fe4c6213b3baec93f19b89173bca25
